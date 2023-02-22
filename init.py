dbda251e5a99ba3e05417fa48ac13afad0f43f31
Xiaofei Du <xiaofei.du@instill.tech>
2023-02-22 10:36:43 +0000

    chore: refactor and add more pipelines (#1)
    Because
    
    - we want to show more pipelines for supported AI tasks on the demo website
    
    This commit
    
    - add pipeline for Image Classification
    - add pipeline for Semantic Segmentation 
    - add pipeline for Keypoint Detection
    - add pipeline for Text to Image
    - add pipeline for Text Generation
    - refactor codebase

--- init.py
+++ init.py
@@ -7,25 +7,71 @@ import time
 # VDP backends
 ###############################################################################
 
+
 # TODO: replace with future api-gateway
 ver = "v1alpha"
-backend: typing.Dict[str, str] = {
-    "pipeline": "localhost:8080",
-    "connector": "localhost:8080",
-    "model": "localhost:8080",
-}
+api_backend = "localhost:8080"
+
+
+def create_github_model(repository, model_id, description=""):
+    print("Create a GitHub model: {} from {}".format(model_id, repository))
+    model = requests.get(
+        f'http://{api_backend}/{ver}/models/{model_id}')
+    if model.status_code == 404:
+        model = requests.post(f'http://{api_backend}/{ver}/models', json={
+            "id": model_id,
+            "model_definition": "model-definitions/github",
+            "description": description,
+            "configuration": {
+                "repository": repository
+            },
+        })
+    else:
+        print("Model already exists: ", model_id)
+    print(model.json())
+    print()
+
+
+def deploy_model_instance(model_id, model_instance_id):
+    print("Deploy a {} model instance: {}".format(model_id, model_instance_id))
+    print()
+    deploy_model_inst = requests.post(
+        f'http://{api_backend}/{ver}/models/{model_id}/instances/{model_instance_id}/deploy')
+    print(deploy_model_inst.json())
+    print()
+
+
+def create_sync_http_pipeline(pipeline_id, model_instance_name, description=""):
+    print("Create a pipeline: {}".format(pipeline_id))
+    pipeline = requests.get(
+        f'http://{api_backend}/{ver}/pipelines/{pipeline_id}')
+    if pipeline.status_code == 404:
+        pipeline = requests.post(f'http://{api_backend}/{ver}/pipelines', json={
+            "id": pipeline_id,
+            "description": description,
+            "recipe": {
+                "source": "source-connectors/source-http",
+                "model_instances": [model_instance_name],
+                "destination": f'destination-connectors/destination-http'
+            }
+        })
+    else:
+        print("Pipeline already exists: ", pipeline_id)
+    print(pipeline.json())
+    print()
 
 ###############################################################################
 # Source connector
 ###############################################################################
 
+
 print("\nCreate a source connector:")
 print()
 src_conn_id = "source-http"
 src_conn = requests.get(
-    f'http://{backend["connector"]}/{ver}/source-connectors/{src_conn_id}')
+    f'http://{api_backend}/{ver}/source-connectors/{src_conn_id}')
 if src_conn.status_code == 404:
-    src_conn = requests.post(f'http://{backend["connector"]}/{ver}/source-connectors', json={
+    src_conn = requests.post(f'http://{api_backend}/{ver}/source-connectors', json={
         "id": src_conn_id,
         "source_connector_definition": "source-connector-definitions/source-http",
         "connector": {
@@ -44,9 +90,9 @@ print("Create a destination connector:")
 print()
 dst_conn_id = "destination-http"
 dst_conn = requests.get(
-    f'http://{backend["connector"]}/{ver}/destination-connectors/{dst_conn_id}')
+    f'http://{api_backend}/{ver}/destination-connectors/{dst_conn_id}')
 if dst_conn.status_code == 404:
-    dst_conn = requests.post(f'http://{backend["connector"]}/{ver}/destination-connectors', json={
+    dst_conn = requests.post(f'http://{api_backend}/{ver}/destination-connectors', json={
         "id": dst_conn_id,
         "destination_connector_definition": "destination-connector-definitions/destination-http",
         "connector": {
@@ -58,284 +104,128 @@ print(dst_conn.json())
 print()
 
 ###############################################################################
-# Model
+# GitHub Model
 ###############################################################################
 
-print("Create a YOLOv4 model:")
+print("###################### Create GitHub models and SYNC HTTP pipelines ######################")
 print()
-model_id = "yolov4"
-model = requests.get(
-    f'http://{backend["model"]}/{ver}/models/{model_id}')
-if model.status_code == 404:
-    model = requests.post(f'http://{backend["model"]}/{ver}/models', json={
-        "id": model_id,
-        "model_definition": "model-definitions/github",
-        "description": "YOLOv4 model imported from GitHub",
-        "configuration": {
-            "repository": "instill-ai/model-yolov4-dvc"
+github: typing.Dict[str, typing.List[typing.Dict[str, str]]] = {
+    "Image Classification": [
+        {
+            "model_id": "mobilenetv2",
+            "repository": "instill-ai/model-mobilenetv2",
+            "model_description": "MobileNetV2 model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "mobilenetv2",
+            "pipeline_description": "A single model sync pipeline for Image Classfication demo with MobileNetV2 model"
+        }
+    ],
+    "Object Detection": [
+        {
+            "model_id": "yolov4",
+            "repository": "instill-ai/model-yolov4-dvc",
+            "model_description": "YOLOv4 model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "yolov4",
+            "pipeline_description": "A single model sync pipeline for Object Detection demo with YOLOv4 model"
         },
-    })
-
-print(model.json())
-print()
-
-time.sleep(5)
-
-print("Deploy a YOLOv4 model instance:")
-print()
-deploy_model_inst = requests.post(
-    f'http://{backend["model"]}/{ver}/models/{model_id}/instances/v1.0-gpu/deploy')
-
-print(deploy_model_inst.json())
-print()
-
-print("Create a YOLOv7 model:")
-print()
-model_id = "yolov7"
-model = requests.get(
-    f'http://{backend["model"]}/{ver}/models/{model_id}')
-if model.status_code == 404:
-    model = requests.post(f'http://{backend["model"]}/{ver}/models', json={
-        "id": model_id,
-        "model_definition": "model-definitions/github",
-        "description": "YOLOv7 model imported from GitHub",
-        "configuration": {
-            "repository": "instill-ai/model-yolov7-dvc"
+        {
+            "model_id": "yolov7",
+            "repository": "instill-ai/model-yolov7-dvc",
+            "model_description": "YOLOv7 model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "yolov7",
+            "pipeline_description": "A single model sync pipeline for Object Detection demo with YOLOv7 model"
+        }
+    ],
+    "Keypoint Detection": [
+        {
+            "model_id": "keypoint",
+            "repository": "instill-ai/model-keypoint-detection-dvc",
+            "model_description": "Detectron2 R-CNN R50-FPN Keypoint model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "keypoint",
+            "pipeline_description": "A single model sync pipeline for Keypoint Detection demo with R-CNN R50-FPN model from Detectron2"
         },
-    })
-
-print(model.json())
-print()
-
-time.sleep(5)
-
-print("Deploy a YOLOv7 model instance:")
-print()
-deploy_model_inst = requests.post(
-    f'http://{backend["model"]}/{ver}/models/{model_id}/instances/v1.0-gpu/deploy')
-
-print(deploy_model_inst.json())
-print()
-
-print("Create an Intsance Segmentation model:")
-print()
-model_id = "instance-segmentation"
-model = requests.get(
-    f'http://{backend["model"]}/{ver}/models/{model_id}')
-if model.status_code == 404:
-    model = requests.post(f'http://{backend["model"]}/{ver}/models', json={
-        "id": model_id,
-        "model_definition": "model-definitions/github",
-        "description": "Instance Segmentation model imported from GitHub",
-        "configuration": {
-            "repository": "instill-ai/model-instance-segmentation-dvc"
+        {
+            "model_id": "yolov7-pose",
+            "repository": "instill-ai/model-yolov7-pose-dvc",
+            "model_description": "YOLOv7 Pose Estimation model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "yolov7-pose",
+            "pipeline_description": "A single model sync pipeline for Keypoint Detection demo with YOLOv7 Pose Estimation model"
+        }
+    ],
+    "OCR": [
+        {
+            "model_id": "ocr",
+            "repository": "instill-ai/model-ocr-dvc",
+            "model_description": "PSNet + EasyOCR Optical Character Recognition (OCR) model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "ocr",
+            "pipeline_description": "A single model sync pipeline for Optical Character Recognition (OCR) demo"
+        }
+    ],
+    "Instance Segmentation": [
+        {
+            "model_id": "instance-segmentation",
+            "repository": "instill-ai/model-instance-segmentation-dvc",
+            "model_description": "Mask RCNN Instance Segmentation model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "instance-segmentation",
+            "pipeline_description": "A single model sync pipeline for Instance Segmentation demo with Mask RCNN model"
         },
-    })
-
-print(model.json())
-print()
-
-time.sleep(5)
-
-print("Deploy an Instance Segmentation model instance:")
-print()
-deploy_model_inst = requests.post(
-    f'http://{backend["model"]}/{ver}/models/{model_id}/instances/v1.0-gpu/deploy')
-
-print(deploy_model_inst.json())
-print()
-
-print("Create an Stomata Instance Segmentation model:")
-print()
-model_id = "stomata-instance-segmentation"
-model = requests.get(
-    f'http://{backend["model"]}/{ver}/models/{model_id}')
-if model.status_code == 404:
-    model = requests.post(f'http://{backend["model"]}/{ver}/models', json={
-        "id": model_id,
-        "model_definition": "model-definitions/github",
-        "description": "Stomata Instance Segmentation model imported from GitHub",
-        "configuration": {
-            "repository": "instill-ai/model-stomata-instance-segmentation-dvc"
-        },
-    })
-
-print(model.json())
-print()
-
-time.sleep(5)
-
-print("Deploy an Stomata Instance Segmentation model instance:")
-print()
-deploy_model_inst = requests.post(
-    f'http://{backend["model"]}/{ver}/models/{model_id}/instances/v2.0-gpu/deploy')
-
-print(deploy_model_inst.json())
-print()
-
-print("Create an OCR model:")
-print()
-model_id = "ocr"
-model = requests.get(
-    f'http://{backend["model"]}/{ver}/models/{model_id}')
-if model.status_code == 404:
-    model = requests.post(f'http://{backend["model"]}/{ver}/models', json={
-        "id": model_id,
-        "model_definition": "model-definitions/github",
-        "description": "OCR model imported from GitHub",
-        "configuration": {
-            "repository": "instill-ai/model-ocr-dvc"
-        },
-    })
-
-print(model.json())
-print()
-
-time.sleep(5)
-
-print("Deploy an OCR model instance:")
-print()
-deploy_model_inst = requests.post(
-    f'http://{backend["model"]}/{ver}/models/{model_id}/instances/v1.0-gpu/deploy')
-
-print(deploy_model_inst.json())
-print()
-
-# print("Create a keypoint model:")
-# print()
-# model_id = "keypoint"
-# model = requests.get(
-#     f'http://{backend["model"]}/{ver}/models/{model_id}')
-# if model.status_code == 404:
-#     model = requests.post(f'http://{backend["model"]}/{ver}/models', json={
-#         "id": model_id,
-#         "model_definition": "model-definitions/github",
-#         "description": "Keypoint model imported from GitHub",
-#         "configuration": {
-#             "repository": "instill-ai/model-keypoint-detection-dvc"
-#         },
-#     })
-
-# print(model.json())
-# print()
-
-# time.sleep(5)
-
-# print("Deploy a keypoint model instance:")
-# print()
-# deploy_model_inst = requests.post(
-#     f'http://{backend["model"]}/{ver}/models/{model_id}/instances/v1.0-gpu/deploy')
-
-# print(deploy_model_inst.json())
-# print()
-
-###############################################################################
-# Pipeline
-###############################################################################
-
-print("Create pipelines:")
-print()
-pipeline_id: typing.Dict[str, str] = {
-    "yolov4": "yolov4",
-    "yolov7": "yolov7",
-    "keypoint": "keypoint",
-    "ocr": "ocr",
-    "instance_segmentation": "instance-segmentation",
-    "stomata": "stomata",
+        {
+            "model_id": "stomata-instance-segmentation",
+            "repository": "instill-ai/model-stomata-instance-segmentation-dvc",
+            "model_description": "Stomata Instance Segmentation model imported from GitHub",
+            "model_instance_id": "v2.0-gpu",
+            "pipeline_id": "stomata",
+            "pipeline_description": "A single model sync pipeline for Stomata Instance Segmentation demo with custom trained Mask RCNN model"
+        }
+    ],
+    "Sementic Segmentation": [
+        {
+            "model_id": "semantic-segmentation",
+            "repository": "instill-ai/model-semantic-segmentation-dvc",
+            "model_description": "Lite R-ASPP based on MobileNetV3 Semantic Segmentation model imported from GitHub",
+            "model_instance_id": "v1.0-gpu",
+            "pipeline_id": "semantic-segmentation",
+            "pipeline_description": "A single model sync pipeline for Semantic Segmentation demo"
+        }
+    ],
+    "Text to Image": [
+        {
+            "model_id": "stable-diffusion",
+            "repository": "instill-ai/model-diffusion-dvc",
+            "model_description": "Stable Diffusion Text to Image model imported from GitHub",
+            "model_instance_id": "fp32-gpu",
+            "pipeline_id": "stable-diffusion",
+            "pipeline_description": "A single model sync pipeline for Text to Image demo with Stable Diffusion model"
+        }
+    ],
+    "Text Generation": [
+        {
+            "model_id": "gpt2",
+            "repository": "instill-ai/model-gpt2-megatron-dvc",
+            "model_description": "GPT2 Text Generation model imported from GitHub",
+            "model_instance_id": "fp32-345m-4-gpus",
+            "pipeline_id": "gpt2",
+            "pipeline_description": "A single model sync pipeline for Text Generation demo with GPT2 model"
+        }
+    ]
 }
-
-pipeline = requests.get(
-    f'http://{backend["pipeline"]}/{ver}/pipelines/{pipeline_id["yolov4"]}')
-if pipeline.status_code == 404:
-    pipeline = requests.post(f'http://{backend["pipeline"]}/{ver}/pipelines', json={
-        "id": pipeline_id["yolov4"],
-        "description": "A single model sync pipeline for YOLOv4 demo",
-        "recipe": {
-            "source": "source-connectors/source-http",
-            "model_instances": ["models/yolov4/instances/v1.0-gpu"],
-            "destination": f'destination-connectors/destination-http'
-        }
-    })
-
-print(pipeline.json())
-print()
-
-pipeline = requests.get(
-    f'http://{backend["pipeline"]}/{ver}/pipelines/{pipeline_id["yolov7"]}')
-if pipeline.status_code == 404:
-    pipeline = requests.post(f'http://{backend["pipeline"]}/{ver}/pipelines', json={
-        "id": pipeline_id["yolov7"],
-        "description": "A single model sync pipeline for YOLOv7 demo",
-        "recipe": {
-            "source": "source-connectors/source-http",
-            "model_instances": ["models/yolov7/instances/v1.0-gpu"],
-            "destination": f'destination-connectors/destination-http'
-        }
-    })
-
-print(pipeline.json())
-print()
-
-pipeline = requests.get(
-    f'http://{backend["pipeline"]}/{ver}/pipelines/{pipeline_id["instance_segmentation"]}')
-if pipeline.status_code == 404:
-    pipeline = requests.post(f'http://{backend["pipeline"]}/{ver}/pipelines', json={
-        "id": pipeline_id["instance_segmentation"],
-        "description": "A single model sync pipeline for Instance Segmentation demo",
-        "recipe": {
-            "source": "source-connectors/source-http",
-            "model_instances": ["models/instance-segmentation/instances/v1.0-gpu"],
-            "destination": f'destination-connectors/destination-http'
-        }
-    })
-
-print(pipeline.json())
-print()
-
-pipeline = requests.get(
-    f'http://{backend["pipeline"]}/{ver}/pipelines/{pipeline_id["stomata"]}')
-if pipeline.status_code == 404:
-    pipeline = requests.post(f'http://{backend["pipeline"]}/{ver}/pipelines', json={
-        "id": pipeline_id["stomata"],
-        "description": "A single model sync pipeline for Stomata Instance Segmentation demo",
-        "recipe": {
-            "source": "source-connectors/source-http",
-            "model_instances": ["models/stomata-instance-segmentation/instances/v2.0-gpu"],
-            "destination": f'destination-connectors/destination-http'
-        }
-    })
-
-print(pipeline.json())
-print()
-
-pipeline = requests.get(
-    f'http://{backend["pipeline"]}/{ver}/pipelines/{pipeline_id["ocr"]}')
-if pipeline.status_code == 404:
-    pipeline = requests.post(f'http://{backend["pipeline"]}/{ver}/pipelines', json={
-        "id": pipeline_id["ocr"],
-        "description": "A single model sync pipeline for OCR demo",
-        "recipe": {
-            "source": "source-connectors/source-http",
-            "model_instances": ["models/ocr/instances/v1.0-gpu"],
-            "destination": f'destination-connectors/destination-http'
-        }
-    })
-
-print(pipeline.json())
-print()
-
-# pipeline = requests.get(
-#     f'http://{backend["pipeline"]}/{ver}/pipelines/{pipeline_id["keypoint"]}')
-# if pipeline.status_code == 404:
-#     pipeline = requests.post(f'http://{backend["pipeline"]}/{ver}/pipelines', json={
-#         "id": pipeline_id["keypoint"],
-#         "description": "A single model sync pipeline for keypoint demo",
-#         "recipe": {
-#             "source": "source-connectors/source-http",
-#             "model_instances": ["models/keypoint/instances/v1.0-gpu"],
-#             "destination": f'destination-connectors/destination-http'
-#         }
-#     })
-
-# print(pipeline.json())
-# print()
+for task, item_ls in github.items():
+    print("###################### {}".format(task))
+    print()
+    for item in item_ls:
+        model_id = item["model_id"]
+        model_instance_id = item["model_instance_id"]
+        create_github_model(item["repository"], model_id,
+                            item["model_description"])
+        time.sleep(5)
+        deploy_model_instance(model_id, model_instance_id)
+        time.sleep(5)
+        model_instance_name = "models/{model_id}/instances/{model_instance_id}"
+        create_sync_http_pipeline(
+            item["pipeline_id"], model_instance_name, item["pipeline_description"])
